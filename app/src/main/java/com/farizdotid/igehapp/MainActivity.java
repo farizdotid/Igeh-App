16ea274884f348de48ae3122c5b8719d49c09e09
farizdotid <contact.farizramadhan@gmail.com>
2017-09-21 16:41:55 +0700

    layout bottom

---
+++ app/src/main/java/com/farizdotid/igehapp/MainActivity.java
@@ -0,0 +1,80 @@
+package com.farizdotid.igehapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.ivHome)
+    ImageView ivHome;
+    @BindView(R.id.ivSearh)
+    ImageView ivSearch;
+    @BindView(R.id.ivAdd)
+    ImageView ivAdd;
+    @BindView(R.id.ivFavorite)
+    ImageView ivFavorite;
+    @BindView(R.id.ivProfile)
+    ImageView ivProfile;
+
+    @BindView(R.id.frameFragment)
+    FrameLayout frameFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        initFragment(new HomeFragment());
+
+        ivHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initFragment(new HomeFragment());
+            }
+        });
+
+        ivSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initFragment(new SearchFragment());
+            }
+        });
+
+        ivAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initFragment(new AddFragment());
+            }
+        });
+
+        ivFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initFragment(new FavoriteFragment());
+            }
+        });
+
+        ivProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initFragment(new ProfileFragment());
+            }
+        });
+    }
+
+    private void initFragment(Fragment classFragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameFragment, classFragment);
+        transaction.commit();
+    }
+}
