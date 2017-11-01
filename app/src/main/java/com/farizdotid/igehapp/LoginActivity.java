32b95ea9b7fc047c0645286acfc9e08575a9ccbd
farizdotid <contact.farizramadhan@gmail.com>
2017-11-01 20:06:08 +0700

    add login page looks like instagram

---
+++ app/src/main/java/com/farizdotid/igehapp/LoginActivity.java
@@ -0,0 +1,40 @@
+package com.farizdotid.igehapp;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.rootLayout) RelativeLayout rootLayout;
+    @BindView(R.id.btnLogin) Button btnLogin;
+    AnimationDrawable animationDrawable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        getSupportActionBar().hide();
+        ButterKnife.bind(this);
+
+        animationDrawable = (AnimationDrawable) rootLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(3000);
+        animationDrawable.setExitFadeDuration(3000);
+        animationDrawable.start();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+    }
+}
